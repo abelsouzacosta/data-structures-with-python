a4ef0b1aa233185a4626c319fe188f36b51154fe
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-10-12 11:43:26 -0300

    [chore]: Fraction constructor

--- src/fraction/Fraction.py
+++ src/fraction/Fraction.py
@@ -1,3 +1,7 @@
+class Fraction:
+    def __init__(self, top, bottom) -> None:
+        self.num = top
+        self.den = bottom
 
     # Prints the fraction in a/b format
     def __str__(self) -> str:
