c3766509c27bf4e76565c82ab09956d6ebc436d5
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-10-13 13:47:46 -0300

    [feat]: Stack

--- src/main.py
+++ src/main.py
@@ -1,37 +1,12 @@
-from algorithms.binary_search.bs import search
+from data_structures.stack.stack import Stack
 
+stack = Stack()
 
-array = [
-    3,
-    7,
-    12,
-    19,
-    25,
-    31,
-    38,
-    44,
-    52,
-    60,
-    71,
-    83,
-    95,
-    108,
-    120,
-    135,
-    150,
-    166,
-    183,
-    201,
-    220,
-    240,
-    261,
-    283,
-    306,
-]
+stack.push(5)
+stack.push(15)
+stack.push(25)
+stack.push(35)
+stack.push(45)
 
-
-print(search(array, 3))
-print(search(array, 95))
-print(search(array, 55))
-print(search(array, 120))
-print(search(array, 166))
+print(stack.pop())  # 45
+print(stack.pop())  # 35
