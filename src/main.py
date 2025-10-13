afce3657b111a406ed8496d650c9e7ed156d5d81
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-10-13 12:58:52 -0300

    [chore]: testing function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,37 @@
-from fraction.Fraction import Fraction
+from algorithms.binary_search.bs import search
 
-myFraction = Fraction(1, 2)
 
-print(myFraction)
+array = [
+    3,
+    7,
+    12,
+    19,
+    25,
+    31,
+    38,
+    44,
+    52,
+    60,
+    71,
+    83,
+    95,
+    108,
+    120,
+    135,
+    150,
+    166,
+    183,
+    201,
+    220,
+    240,
+    261,
+    283,
+    306,
+]
+
+
+print(search(array, 3))
+print(search(array, 95))
+print(search(array, 55))
+print(search(array, 120))
+print(search(array, 166))
