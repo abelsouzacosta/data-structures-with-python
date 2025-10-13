cffaed3d0b3bf928944ee4b33fb633d3f1fc0bc2
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-10-13 13:46:59 -0300

    [chore]: pop
    Adding method that removes and returns the element at the top of the
    stack

--- src/data_structures/stack/stack.py
+++ src/data_structures/stack/stack.py
@@ -10,3 +10,20 @@ class Stack:
     def push(self, element: int) -> None:
         self.items.append(element)
 
+    # removes and return the element at the top of the array
+    def pop(self) -> int:
+        if self.is_empty():
+            raise Exception("Empty Stack")
+        # takes the last element of the array
+        top = self.items[-1]
+        # delete the element
+        del self.items[-1]
+
+        """
+        Python provides the method pop for lists
+        but since the idea here is to learn data structures then 
+        all the process above is made manually, but in 'real life'
+        you can just use the method pop: self.items.pop()
+        """
+        return top
+
