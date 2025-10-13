e4f35d985c4ffbe8b72e1a7402bed5d810223374
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-10-13 15:15:31 -0300

    [chore]: LinkedList: insert_at_head
    Adding a method to insert a element at the head of the linked list

--- src/data_structures/linked_list/linked_list.py
+++ src/data_structures/linked_list/linked_list.py
@@ -5,3 +5,16 @@ class LinkedList:
     def __init__(self) -> None:
         self.head = None
         self.tail = None
+
+    # Insert an element at the head
+    def insert_at_head(self, data) -> None:
+        node = Node(data)
+
+        if self.head is not None:
+            old_head = self.head
+            self.head = node
+            node.next = old_head
+        else:
+            self.head = node
+            self.tail = node
+
