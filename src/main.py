95b25c717b1f9f2b1863061588f705190956a438
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-10-13 16:31:46 -0300

    [chore]: LinkedList

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from data_structures.stack.stack import Stack
+from data_structures.linked_list.linked_list import LinkedList
 
 stack = Stack()
 
@@ -10,3 +11,15 @@ stack.push(45)
 
 print(stack.pop())  # 45
 print(stack.pop())  # 35
+
+my_list = LinkedList()
+
+my_list.insert_at_head(1)
+my_list.insert_at_head(2)
+my_list.insert_at_head(3)
+my_list.insert_at_head(4)
+my_list.insert_at_head(5)
+my_list.insert_at_tail(10)
+my_list.insert_after_element(5, 6)
+
+my_list.print_list()
