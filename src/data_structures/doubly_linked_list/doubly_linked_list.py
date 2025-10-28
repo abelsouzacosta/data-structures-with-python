8ebfb1c24541b20c05bf1b18d362f05a1576b4e5
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-10-28 16:58:38 -0300

    [feat]: insert_at_head
    Adding method that allows to insert a new node on the head of the doubly
    linked list

--- src/data_structures/doubly_linked_list/doubly_linked_list.py
+++ src/data_structures/doubly_linked_list/doubly_linked_list.py
@@ -7,3 +7,16 @@ class DoublyLinkedList:
         self.head: Optional[Node] = None
         self.head: Optional[Node] = None
 
+    def insert_at_head(self, data) -> None:
+        node = Node(data)
+
+        if self.head is None:
+            self.head = node
+            self.tail = node
+            return
+
+        node.next = self.head
+        self.head.previous = node
+        self.head = node
+        return
+
