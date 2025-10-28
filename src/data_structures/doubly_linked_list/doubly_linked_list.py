10373f111ea34b6d6769f19ccc20d2ed4202e8b8
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-10-28 16:57:10 -0300

    [chore]: DoublyLinkedList
    Adding constructor for doubly linked list

---
+++ src/data_structures/doubly_linked_list/doubly_linked_list.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from .node import Node
+
+
+class DoublyLinkedList:
+    def __init__(self) -> None:
+        self.head: Optional[Node] = None
+        self.head: Optional[Node] = None
+
