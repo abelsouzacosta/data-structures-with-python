085529584cc075246f3f64a9eb2d3616ef813f40
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-11-02 16:16:19 -0300

    [chore]: pop
    Adding method to remove and return the element at the top of the stack

--- src/data_structures/dynamicstack/dynamicstack.py
+++ src/data_structures/dynamicstack/dynamicstack.py
@@ -23,3 +23,12 @@ class DynamicStack:
     def push(self, data: int) -> None:
         # insert a element in the head of the linked list
         self.elements.insert_at_head(data)
+
+    def pop(self) -> Optional[int]:
+        value = self.elements.get_head()
+        if value is not None:
+            # updates the list removing the element in the head
+            self.elements.delete_from_head()
+            return value
+        return
+
